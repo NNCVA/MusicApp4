c8f3665134d598dee78379f6af8bed7730bb9cce
NNCVA <vanc6612@outlook.com>
2026-09-20 22:23:41 +0800

    build:1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.musicapp4.player"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
         testInstrumentationRunner = "com.musicapp.player.HiltTestRunner"
     }
 
